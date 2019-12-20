3dc7bec27450fdf9ed8188ba03b1ed45ecba4864
chuchong <33688310+chuchong@users.noreply.github.com>
2019-12-21 02:01:03 +0800

    modify 测试的c函数

--- test.c
+++ test.c
@@ -1,1 +1,25 @@
-    {i=1;while(i<100+32)i=i+i;}
+
+int testMulti(){
+//    test(1,1,1);
+    int i = 1;
+    int j = i ++;
+    int k = ++ i;
+    int u = k --;
+    int v = -- k;
+    int w = -v;
+    printf("%d%d%d%d%d%d",i,j,k,u,v,w);
+    return 0;
+}
+
+int main(){
+    for(int i = 1; i < 10; i++){
+        printf("%d\n", i);
+        i = i + 1;
+    }
+    testMulti();
+    return 0;
+}
+
+//int test(int i, int j, int k){
+//    return 0;
+//}
